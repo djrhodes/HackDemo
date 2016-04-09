ba2098ab4895a0ac8914bfa749987bd36921a24f
jrhodes <jrhodes.home@gmail.com>
2016-04-10 00:10:53 +0100

    homecontroller + view

--- src/HackDemo/Startup.cs
+++ src/HackDemo/Startup.cs
@@ -15,17 +15,24 @@ namespace HackDemo
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
+            app.UseStaticFiles();
+            app.UseMvc(config =>
+                {
+                    config.MapRoute(
+                        name: "Default",
+                        template: "{controller}/{action}/{id?}",
+                        defaults: new {  controller = "Home", action = "Index" }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                    );
+                }
+            
+            );
         }
 
         // Entry point for the application.
